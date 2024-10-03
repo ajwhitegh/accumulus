61372ab77bb343b4717c690f57c1f87af7e7c7f2
ajwhitegh <149695015+ajwhitegh@users.noreply.github.com>
2024-10-03 11:08:41 -0400

    Create AccumulusSynergyCodingChallengeApplication.kt

---
+++ src/main/kotlin/com/ascc/accumulussynergycodingchallenge/AccumulusSynergyCodingChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.ascc.accumulussynergycodingchallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccumulusSynergyCodingChallengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccumulusSynergyCodingChallengeApplication>(*args)
+}
